7b4e42aebcf32d52dc7f4a143d62fb76a16daa8f
André N. Darcie <andrendarcie@gmail.com>
2017-11-17 23:14:06 -0200

    Add username password and host for development database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171117011329) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.datetime "year"
