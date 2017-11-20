031afe58c9bb711a000921f3d16b7ced547432c9
André N. Darcie <andrendarcie@gmail.com>
2017-11-19 23:37:33 -0200

    Add Facebook Omniauth configuration for users

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,6 +252,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  # Facebook configurations
+  config.omniauth :facebook, "681500895381581", "ec00ccc0d053f59ebcc744ae64f5f79c", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
