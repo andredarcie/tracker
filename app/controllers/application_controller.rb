ef3dc9ff1a391401d9829242ae51fcd78e7e1bbe
Fabricio Damazio <fabridamazio@hotmail.com>
2017-11-30 00:08:50 -0200

    Add static_pages template check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
+  def after_sign_in_path_for(resource)
+    movies_path
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "static_pages"
+    else
+      "application"
+    end
+  end
+
 end
