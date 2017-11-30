2089372d8fae7fa1416e0d63740cfb223a4b7b2b
Fabricio Damazio <fabridamazio@hotmail.com>
2017-11-30 00:09:38 -0200

    Fix devise auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root 'movies#index'
-  resources :movies
+
+  devise_for :users
+
+  authenticate :user do
+    resources :movies
+  end
+
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
